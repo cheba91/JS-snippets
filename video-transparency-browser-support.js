7d46f8ab390f87fbe3771dd4b6d1ec45d2c2339c
cheba91 <58878791+cheba91@users.noreply.github.com>
2023-06-22 13:32:43 +0200

    Update video-transparency-browser-support.js

--- video-transparency-browser-support.js
+++ video-transparency-browser-support.js
@@ -10,6 +10,6 @@
   const isIE11 = () => !!window.navigator.userAgent.match(/Trident\/7\./, []);
 
   if (!isIE11()) {
-    const player = document.querySelector('.,y-player');
+    const player = document.querySelector('.my-player');
     player.src = supportsHEVCAlpha() ? 'https://doggo.s3.amazonaws.com/output.mov' : 'https://doggo.s3.amazonaws.com/output.webm';
   }
