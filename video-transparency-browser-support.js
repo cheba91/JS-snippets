8b1df573fc71ff28546e1dd1d832398440d91366
cheba91 <58878791+cheba91@users.noreply.github.com>
2023-06-22 13:32:18 +0200

    Create video-transparency-browser-support.js

---
+++ video-transparency-browser-support.js
@@ -0,0 +1,15 @@
+  const supportsHEVCAlpha = () => {
+    const navigator = window.navigator;
+    const ua = navigator.userAgent.toLowerCase();
+    const hasMediaCapabilities = !!(navigator.mediaCapabilities && navigator.mediaCapabilities.decodingInfo);
+    const isSafari = ua.indexOf('safari') != -1 && !(ua.indexOf('chrome') != -1) && ua.indexOf('version/') != -1;
+
+    return isSafari && hasMediaCapabilities;
+  };
+
+  const isIE11 = () => !!window.navigator.userAgent.match(/Trident\/7\./, []);
+
+  if (!isIE11()) {
+    const player = document.querySelector('.,y-player');
+    player.src = supportsHEVCAlpha() ? 'https://doggo.s3.amazonaws.com/output.mov' : 'https://doggo.s3.amazonaws.com/output.webm';
+  }
