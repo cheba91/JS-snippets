b53d8fbd1fbaf365c30a76bc9db29981bc9dfd8c
cheba91 <58878791+cheba91@users.noreply.github.com>
2023-07-24 09:31:53 +0200

    Create timer-countdown.js

---
+++ timer-countdown.js
@@ -0,0 +1,35 @@
+const daysEl = document.querySelector('.countdown-timer-container #days');
+const hoursEl = document.querySelector('.countdown-timer-container #hours');
+const minutesEl = document.querySelector('.countdown-timer-container #minutes');
+const secondsEl = document.querySelector('.countdown-timer-container #seconds');
+
+const countdownDate = new Date('July 29, 2023 12:30 AM EST').getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // always show with 2 digits
+  days = days.toString().padStart(2, '0');
+  hours = hours.toString().padStart(2, '0');
+  minutes = minutes.toString().padStart(2, '0');
+  seconds = seconds.toString().padStart(2, '0');
+
+  daysEl.textContent = days;
+  hoursEl.textContent = hours;
+  minutesEl.textContent = minutes;
+  secondsEl.textContent = seconds;
+
+  // After the countdown has finished
+  if (distance < 0) {
+    daysEl.textContent = '00';
+    hoursEl.textContent = '00';
+    minutesEl.textContent = '00';
+    secondsEl.textContent = '00';
+  }
+}, 1000);
