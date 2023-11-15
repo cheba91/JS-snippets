6366eca5bec95b7e06b90c9ba3ab1a04f785fc26
cheba91 <58878791+cheba91@users.noreply.github.com>
2023-11-15 15:54:54 +0100

    Update load-GTM-on-interaction.js

--- load-GTM-on-interaction.js
+++ load-GTM-on-interaction.js
@@ -1,5 +1,5 @@
-const createGTMscript = () => {
-  if (window.scriptCreated) return;
+const loadScriptsOnInteract = () => {
+  if (window.scriptsCreated) return;
   var w = window,
     d = document,
     s = 'script',
@@ -14,14 +14,11 @@ const createGTMscript = () => {
   j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
   f.parentNode.insertBefore(j, f);
   console.log('GTM script created');
-  window.scriptCreated = true;
+  window.scriptsCreated = true;
+  // Other scripts that need to be loaded on interact
 };
 
 window.addEventListener('load', () => {
-  ['mousemove', 'touchstart'].forEach((e) => {
-    window.addEventListener(e, () => {
-      createGTMscript();
-    });
-  });
-  // Other scripts
+  ['mousemove', 'touchstart'].forEach((ev) => window.addEventListener(ev, loadScriptsOnInteract));
+  // Other script that need only "load" event
 });
