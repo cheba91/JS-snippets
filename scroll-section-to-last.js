38969a382b1de1229849486854f2c5378f14e189
cheba91 <58878791+cheba91@users.noreply.github.com>
2024-12-17 15:13:49 +0100

    Create scroll-section-to-last.js

---
+++ scroll-section-to-last.js
@@ -0,0 +1,24 @@
+
+  window.addEventListener('load', () => {
+    const section = document.querySelector('.scroll-section');
+    const lastElement = document.querySelector('.blue-card[scroll-element="last"]');
+
+    const sectionRect = section.getBoundingClientRect();
+    const lastElementRect = lastElement.getBoundingClientRect();
+    const totalMove = lastElementRect.right - sectionRect.right;
+
+    window.addEventListener('scroll', () => {
+      const sectionRect = section.getBoundingClientRect();
+      const lastElementRect = lastElement.getBoundingClientRect();
+
+      let progress = 0;
+      if (sectionRect.top <= 0) {
+        const rawProgress = Math.abs(sectionRect.top) / (section.offsetHeight - window.innerHeight);
+        progress = Math.min(1, rawProgress);
+      }
+
+      const moveX = totalMove * progress;
+      lastElement.parentElement.style.transform = `translateX(-${moveX}px)`;
+    });
+  });
+
