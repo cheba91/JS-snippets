3626ef89ec38d837f203a3dd7dcf8aa1046ce1c9
cheba91 <58878791+cheba91@users.noreply.github.com>
2025-09-09 17:26:08 +0200

    Update load-GTM-scripts-on-interaction.js

--- load-GTM-scripts-on-interaction.js
+++ load-GTM-scripts-on-interaction.js
@@ -1,40 +1,37 @@
-const scriptsToLoad = [];
-// Load on interect
-const loadScriptsOnInteract = () => {
-  if (window.scriptsCreated) return;
-  // load GTM
-  var w = window,
-    d = document,
-    s = 'script',
-    l = 'dataLayer',
-    i = 'GTM-0000000';
-  w[l] = w[l] || [];
-  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-  var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s),
-    dl = l != 'dataLayer' ? '&l=' + l : '';
-  j.async = true;
-  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-  f.parentNode.insertBefore(j, f);
+  const scriptsToLoad = [];
+  // Load on interaction
+  const loadScriptsOnInteract = () => {
+    // load GTM
+    var w = window,
+      d = document,
+      s = 'script',
+      l = 'dataLayer',
+      i = 'GTM-0000000';
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
 
-  // Load other scripts
-  scriptsToLoad.forEach((src) => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = src;
-    script.async = false;
-    document.body.appendChild(script);
+    // Load other scripts
+    scriptsToLoad.forEach((src) => {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = src;
+      script.async = false;
+      document.body.appendChild(script);
+    });
+
+    console.log('interaction scripts created');
+  };
+
+  window.addEventListener('load', () => {
+    ['mousemove', 'touchstart'].forEach((ev) => window.addEventListener(ev, loadScriptsOnInteract, { once: true }));
+    // Other script that need only "load" event
   });
-
-  window.scriptsCreated = true;
-  console.log('interaction scripts created');
-};
-
-window.addEventListener('load', () => {
-  ['mousemove', 'touchstart'].forEach((ev) => window.addEventListener(ev, loadScriptsOnInteract));
-  //     ['mousemove', 'touchstart'].forEach((ev) => window.addEventListener(ev, loadScriptsOnInteract, { once: true }));
-  // Other script that need only "load" event
-});
 
 // Load just HS script
 window.addEventListener('load', () => {
